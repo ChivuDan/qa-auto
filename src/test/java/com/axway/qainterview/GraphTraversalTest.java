6010216eacb7e6ce79c4eb8629fde3ae66273017
CHIVU\Chivu <chivudan.n@gmail.com>
2019-08-03 14:22:18 +0300

    Working Unit Tests

---
+++ src/test/java/com/axway/qainterview/GraphTraversalTest.java
@@ -0,0 +1,10 @@
+package com.axway.qainterview;
+
+import org.junit.Test;
+
+public class GraphTraversalTest {
+    @Test
+    public void testPoC(){
+        System.out.println("test are config and working");
+    }
+}
